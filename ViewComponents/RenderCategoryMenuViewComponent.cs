7ee927299ddd282ab860a87ef823b33281b53aba
MHG1503 <mhg1503@gmail.com>
2023-10-23 23:05:20 +0700

    update interface

---
+++ ViewComponents/RenderCategoryMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOOK_STORE_DEMO.ViewComponents
+{
+    public class RenderCategoryMenuViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderCategories");
+        }
+    }
+}
---
+++ ViewComponents/RenderPaginationViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BOOK_STORE_DEMO.ViewComponents
+{
+    public class RenderPaginationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderPaginate");
+        }
+    }
+}
